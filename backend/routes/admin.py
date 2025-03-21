87edf1825066485a31f9a1f2667706448973d6d2
Rahul Maurya <78677484+k26rahul@users.noreply.github.com>
2025-03-21 22:22:37 +0530

    quiz & questions route

--- backend/routes/admin.py
+++ backend/routes/admin.py
@@ -1,10 +1,171 @@
 from flask import Blueprint, jsonify, request
 from flask_security import roles_required
-from db.models import db, Subject, Chapter
+from db.models import db, Subject, Chapter, Quiz, Question
 
 admin_bp = Blueprint('admin', __name__)
 
+# Quiz Routes
+
+
+@admin_bp.route('/get-quiz/<int:id>', methods=['GET'])
+@roles_required('admin')
+def get_quiz(id):
+  quiz = Quiz.query.get_or_404(id)
+  quiz_dict = quiz.as_dict()
+  quiz_dict['chapter_name'] = quiz.chapter.name
+  quiz_dict['subject_name'] = quiz.chapter.subject.name
+  quiz_dict['questions'] = [q.as_dict() for q in quiz.questions]
+  return jsonify(success=True, quiz=quiz_dict)
+
+
+@admin_bp.route('/get-quizzes', methods=['GET'])
+@roles_required('admin')
+def get_quizzes():
+  quizzes = Quiz.query.all()
+  result = []
+  for quiz in quizzes:
+    quiz_dict = quiz.as_dict()
+    quiz_dict['chapter_name'] = quiz.chapter.name
+    quiz_dict['subject_name'] = quiz.chapter.subject.name
+    result.append(quiz_dict)
+  return jsonify(success=True, quizzes=result)
+
+
+@admin_bp.route('/create-quiz', methods=['POST'])
+@roles_required('admin')
+def create_quiz():
+  data = request.get_json()
+  title = data.get('title')
+  description = data.get('description')
+  chapter_id = data.get('chapter_id')
+  start_time = data.get('start_time')
+  duration = data.get('duration')
+
+  if not all([title, chapter_id]):
+    return jsonify(success=False, message="Quiz title and chapter_id are required"), 400
+
+  chapter = Chapter.query.get(chapter_id)
+  if not chapter:
+    return jsonify(success=False, message=f"Chapter with id {chapter_id} not found"), 404
+
+  quiz = Quiz(
+      title=title,
+      description=description,
+      chapter_id=chapter_id,
+      start_time=start_time,
+      duration=duration
+  )
+  db.session.add(quiz)
+  db.session.commit()
+
+  return jsonify(success=True, quiz=quiz.as_dict(), message="Quiz created successfully")
+
+
+@admin_bp.route('/update-quiz/<int:id>', methods=['PUT'])
+@roles_required('admin')
+def update_quiz(id):
+  quiz = Quiz.query.get_or_404(id)
+  data = request.get_json()
+
+  quiz.title = data.get('title', quiz.title)
+  quiz.description = data.get('description', quiz.description)
+  quiz.start_time = data.get('start_time', quiz.start_time)
+  quiz.duration = data.get('duration', quiz.duration)
+
+  db.session.commit()
+  return jsonify(success=True, quiz=quiz.as_dict())
+
+
+@admin_bp.route('/delete-quiz/<int:id>', methods=['DELETE'])
+@roles_required('admin')
+def delete_quiz(id):
+  quiz = Quiz.query.get_or_404(id)
+  db.session.delete(quiz)
+  db.session.commit()
+  return jsonify(success=True, message="Quiz deleted successfully")
+
+
+# Question Routes
+@admin_bp.route('/get-question/<int:id>', methods=['GET'])
+@roles_required('admin')
+def get_question(id):
+  question = Question.query.get_or_404(id)
+  question_dict = question.as_dict()
+  question_dict['quiz_title'] = question.quiz.title
+  question_dict['chapter_name'] = question.quiz.chapter.name
+  question_dict['subject_name'] = question.quiz.chapter.subject.name
+  return jsonify(success=True, question=question_dict)
+
+
+@admin_bp.route('/create-question', methods=['POST'])
+@roles_required('admin')
+def create_question():
+  data = request.get_json()
+  statement = data.get('statement')
+  option_a = data.get('option_a')
+  option_b = data.get('option_b')
+  option_c = data.get('option_c')
+  option_d = data.get('option_d')
+  correct_option = data.get('correct_option')
+  quiz_id = data.get('quiz_id')
+
+  if not all([statement, option_a, option_b, option_c, option_d, correct_option, quiz_id]):
+    return jsonify(success=False, message="All question fields are required"), 400
+
+  quiz = Quiz.query.get(quiz_id)
+  if not quiz:
+    return jsonify(success=False, message=f"Quiz with id {quiz_id} not found"), 404
+
+  question = Question(
+      statement=statement,
+      option_a=option_a,
+      option_b=option_b,
+      option_c=option_c,
+      option_d=option_d,
+      correct_option=correct_option,
+      quiz_id=quiz_id
+  )
+  db.session.add(question)
+  db.session.commit()
+
+  return jsonify(success=True, question=question.as_dict(), message="Question created successfully")
+
+
+@admin_bp.route('/update-question/<int:id>', methods=['PUT'])
+@roles_required('admin')
+def update_question(id):
+  question = Question.query.get_or_404(id)
+  data = request.get_json()
+
+  question.statement = data.get('statement', question.statement)
+  question.option_a = data.get('option_a', question.option_a)
+  question.option_b = data.get('option_b', question.option_b)
+  question.option_c = data.get('option_c', question.option_c)
+  question.option_d = data.get('option_d', question.option_d)
+  question.correct_option = data.get('correct_option', question.correct_option)
+
+  db.session.commit()
+  return jsonify(success=True, question=question.as_dict())
+
+
+@admin_bp.route('/delete-question/<int:id>', methods=['DELETE'])
+@roles_required('admin')
+def delete_question(id):
+  question = Question.query.get_or_404(id)
+  db.session.delete(question)
+  db.session.commit()
+  return jsonify(success=True, message="Question deleted successfully")
+
 # Subject Routes
+
+
+@admin_bp.route('/get-subject/<int:id>', methods=['GET'])
+@roles_required('admin')
+def get_subject(id):
+  subject = Subject.query.get_or_404(id)
+  subject_dict = subject.as_dict()
+  subject_dict['chapters'] = [chapter.as_dict() for chapter in subject.chapters]
+  return jsonify(success=True, subject=subject_dict)
 
 
 @admin_bp.route('/get-subjects', methods=['GET'])
@@ -65,16 +226,14 @@ def delete_subject(id):
   return jsonify(success=True, message="Subject deleted successfully")
 
 
-@admin_bp.route('/get-subject/<int:id>', methods=['GET'])
-@roles_required('admin')
-def get_subject(id):
-  subject = Subject.query.get_or_404(id)
-  subject_dict = subject.as_dict()
-  subject_dict['chapters'] = [chapter.as_dict() for chapter in subject.chapters]
-  return jsonify(success=True, subject=subject_dict)
-
-
 # Chapter Routes
+
+@admin_bp.route('/get-chapter/<int:id>', methods=['GET'])
+@roles_required('admin')
+def get_chapter(id):
+  chapter = Chapter.query.get_or_404(id)
+  chapter_dict = chapter.as_dict()
+  return jsonify(success=True, chapter=chapter_dict)
 
 
 @admin_bp.route('/create-chapter', methods=['POST'])
@@ -133,11 +292,3 @@ def delete_chapter(id):
   db.session.delete(chapter)
   db.session.commit()
   return jsonify(success=True, message="Chapter deleted successfully")
-
-
-@admin_bp.route('/get-chapter/<int:id>', methods=['GET'])
-@roles_required('admin')
-def get_chapter(id):
-  chapter = Chapter.query.get_or_404(id)
-  chapter_dict = chapter.as_dict()
-  return jsonify(success=True, chapter=chapter_dict)
